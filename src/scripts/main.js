790ebee3daced6544d31717c4b2f4b009e68bb9d
Шелков Дмитрий <bobyorrr@gmail.com>
2020-09-11 11:47:21 +0300

    адаптив header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 // Бургер меню
-const burgerButton = document.querySelectorAll(".main-manu__burger-button");
-const burgerManu = document.querySelector(".burger-manu");
+const burgerButton = document.querySelectorAll(".main-menu__burger-button");
+const burgerMenu = document.querySelector(".burger-menu");
 
 for (const i of burgerButton) {
 	i.addEventListener("click", () => {
-		burgerManu.classList.toggle("burger-manu__is-active");
+		burgerMenu.classList.toggle("burger-menu__is-active");
 		for (const b of burgerButton) {
-			b.classList.toggle("main-manu__burger-button-cross");
+			b.classList.toggle("main-menu__burger-button-cross");
 		}
 	});
 }
